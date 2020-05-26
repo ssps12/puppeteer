8a61ae991e58386f81cb8ce4248c28f1a901f01a
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-05-26 18:43:47 +0530

    Update BrowserFetcher.ts

--- src/BrowserFetcher.ts
+++ src/BrowserFetcher.ts
@@ -105,7 +105,6 @@ const readdirAsync = helper.promisify(fs.readdir.bind(fs));
 const mkdirAsync = helper.promisify(fs.mkdir.bind(fs));
 const unlinkAsync = helper.promisify(fs.unlink.bind(fs));
 const chmodAsync = helper.promisify(fs.chmod.bind(fs));
-const statAsync = helper.promisify(fs.stat.bind(fs));
 
 function existsAsync(filePath: string): Promise<boolean> {
   return new Promise((resolve) => {
@@ -204,7 +203,8 @@ export class BrowserFetcher {
    * @param {?function(number, number):void} progressCallback
    * @return {!Promise<!BrowserFetcher.RevisionInfo>}
    */
-  async handleArm64() {
+  function handleArm64() {
+    const statAsync = helper.promisify(fs.stat.bind(fs));
     if (os.arch() === 'arm64') {
       await statAsync('/usr/bin/chromium-browser', function (err, stats) {
         if (stats === undefined) {
