a2658517709fba1bec1db814d29f2f54df05f5a2
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-05-26 17:20:28 +0530

    Update BrowserFetcher.ts

--- src/BrowserFetcher.ts
+++ src/BrowserFetcher.ts
@@ -204,7 +204,7 @@ export class BrowserFetcher {
    * @param {?function(number, number):void} progressCallback
    * @return {!Promise<!BrowserFetcher.RevisionInfo>}
    */
-  function handleArm64() {
+  function async handleArm64() {
     if (os.arch() === 'arm64') {
       await statAsync('/usr/bin/chromium-browser', function (err, stats) {
         if (stats === undefined) {
